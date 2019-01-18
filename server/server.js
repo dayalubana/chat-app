dd5852fe5146858ab1e97cb0087031cf6b7a0fd3
dayalubana <daya16001001017@gmail.com>
2019-01-18 19:52:36 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const path=require('path')
+const express=require('express');
+var publicPath=path.join(__dirname,'../public')
+const port=process.env.Port||3000
+var app=express();
+
+app.use(express.static(publicPath))
+
+app.listen(port)
