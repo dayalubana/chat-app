f206b452b0989f979d7ae0d7c8203ac4cd6affa6
dayalubana <46818015+dayalubana@users.noreply.github.com>
2019-01-18 20:23:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ var publicPath=path.join(__dirname,'../public')
 const port=process.env.PORT||3000
 var app=express();
 
-app.use(express.static(publicPath))
+app.use(express.static(__dirname,'/../public'))
 
 app.listen(port)
