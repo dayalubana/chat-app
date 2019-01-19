d946ce5bbacb5c3c561ea4fc80ef62410822310f
dayalubana <daya16001001017@gmail.com>
2019-01-19 19:46:42 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const path=require('path')
 const express=require('express');
+const socketIO=require('socket.io');
+const http=require('http')
 var publicPath=path.join(__dirname,'../public')
-const port=process.env.Port||3000
+const port=process.env.PORT||3000
 var app=express();
 
-app.use(express.static(publicPath))
+app.use(express.static(__dirname+'/../public'))
 
-app.listen(port)
+var server=http.createServer(app)
+var io=socketIO(server)
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+  socket.on('disconnect',()=>{
+    console.log('user was disconnected')
+  })
+})
+server.listen(port)
